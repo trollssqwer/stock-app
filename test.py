bae71124d09305e7cc6022ee4413619f2a5affbc
tranthong <trollssqwer@gmail.com>
2023-03-07 15:58:41 +0700

    s

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import time
 import MetaTrader5 as mt5
+import pandas as pd
 
 print("MetaTrader5 package author: ", mt5.__author__)
 print("MetaTrader5 package version: ", mt5.__version__)
@@ -8,3 +9,20 @@ print("MetaTrader5 package version: ", mt5.__version__)
 if not mt5.initialize(login=113808435, server="Exness-MT5Trial6",password="Tranthong98"):
     print("initialize() failed, error code =",mt5.last_error())
     quit()
+
+import re
+import os
+path = '/Users/tranthong/Desktop/stock-app/stock_data_M5/'
+list_stock = []
+for file_name in os.listdir(path):
+  list_stock.append(re.search(r"(.+)\_.+" ,file_name).group(1))
+list_stock_new = list(dict.fromkeys(list_stock))
+list_stock_new
+test_df = pd.DataFrame(columns=['ticker', 'point', 'spread' , 'deviation'])
+for ticker in list_stock_new:
+    point = mt5.symbol_info(ticker).point
+    spread = mt5.symbol_info(ticker).spread + 1
+    row = {'ticker': ticker,'point': point,'spread': spread,'deviation': 5}
+    test_df.append(row)
+
+print('ok')
