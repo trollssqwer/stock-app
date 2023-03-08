a049ed4d5818affe0609f90a9a2999a78d25865d
tranthong <trollssqwer@gmail.com>
2023-03-08 15:59:54 +0700

    s

--- WyckOff2.py
+++ WyckOff2.py
@@ -876,7 +876,7 @@ print("MetaTrader5 package author: ", mt5.__author__)
 print("MetaTrader5 package version: ", mt5.__version__)
 
 # establish connection to the MetaTrader 5 terminal
-if not mt5.initialize(login=113808435, server="Exness-MT5Trial6",password="Tranthong98"):
+if not mt5.initialize(login=114057554, server="Exness-MT5Trial6",password="Tranthong98"):
     print("initialize() failed, error code =",mt5.last_error())
     quit()
 
